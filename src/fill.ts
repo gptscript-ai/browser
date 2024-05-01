85b9f5df6635e824e5214517a251854e16b6e894
Grant Linville <grant@acorn.io>
2024-05-01 09:16:25 -0400

    Browser improvements
    Signed-off-by: Grant Linville <grant@acorn.io>

--- src/fill.ts
+++ src/fill.ts
@@ -1,7 +1,7 @@
 import { delay } from './delay'
 import { type BrowserContext } from 'playwright'
 import { Mutex } from 'async-mutex'
-import { inspect, getText } from './browse'
+import { inspect } from './browse'
 
 const mutex = new Mutex()
 
@@ -9,24 +9,21 @@ export async function fill (context: BrowserContext, userInput: string, content:
   const release = await mutex.acquire()
   try {
     const locators = await inspect(context, userInput, 'fill', keywords)
-    outer:
     for (const locator of locators) {
       console.log(locator)
       try {
         const pages = context.pages()
         const page = pages[pages.length - 1]
-        const elements = await page.locator(`${locator}`).all()
-        for (const element of elements) {
-          const innerText = await getText(element)
-          for (const keyword of keywords) {
-            if (innerText.toLowerCase().includes(keyword.toLowerCase())) {
-              await element.fill(content)
-              await delay(5000)
-              break outer
-            }
-          }
+        const elements = await page.locator(locator).all()
+        if (elements.length === 0) {
+          continue
         }
+
+        await elements[0].fill(content)
+        await delay(5000)
+        break
       } catch (e) {
+        console.error(e)
       }
     }
   } finally {
