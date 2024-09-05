a5ca63012e9205853e9e23839c77914e1e63f9d6
Grant Linville <grant@acorn.io>
2024-09-05 16:46:20 -0400

    enhance: override browser settings location with environment variable (#39)
    Signed-off-by: Grant Linville <grant@acorn.io>

--- src/settings.ts
+++ src/settings.ts
@@ -7,6 +7,15 @@ export interface BrowserSettings {
 }
 
 export function loadSettingsFile (): BrowserSettings {
+  if (process.env.GPTSCRIPT_BROWSER_SETTINGS_FILE != null && process.env.GPTSCRIPT_BROWSER_SETTINGS_FILE !== '') {
+    try {
+      const contents = fs.readFileSync(process.env.GPTSCRIPT_BROWSER_SETTINGS_FILE)
+      return JSON.parse(contents.toString())
+    } catch (e) {
+      return {}
+    }
+  }
+
   const file = process.env.GPTSCRIPT_WORKSPACE_DIR + '/browsersettings.json'
   try {
     const contents = fs.readFileSync(file)
